589ad1052109f7d1a50951855a788ab77d4c7076
ybgirgin3 <ybgirgin3@gmail.com>
2021-06-11 21:25:09 +0300

    asd

--- GizemOZDEN/Kubra/UBUNTU/calisanlar/NDVI-analizi-Django-project/accounts/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username=forms.CharField(max_length=100, label="kullanıcı adi")
-    password= forms.CharField(max_length=100, label="şifre", widget=forms.PasswordInput)
--- GizemOZDEN/Kubra/UBUNTU/calisanlar/NDVI-analizi-Django-project/accounts/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .forms import LoginForm
-# Create your views here.
-
-def login_view(request):
-    form= LoginForm(rquest.Post or none)
-    reurnt render(request)
